9ef1fd30323361f784cf4e279f62c922bc6f975a
pajlada <rasmus.karlsson@pajlada.com>
2018-12-16 16:55:43 +0100

    Update main.go

--- main.go
+++ main.go
@@ -5,3 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println(fmt.Sprintf("do you like that fixmie %s"))
 }
+
+func getCoolError() error {
+	return errors.New(fmt.Sprintf("something was wrong.... %s", lol))
+}
